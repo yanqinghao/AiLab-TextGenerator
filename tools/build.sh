3dd90ce6ebf556a0002f77b9dea9e3827724c066
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-20 10:52:45 +0800

    fix docker file

--- tools/build.sh
+++ tools/build.sh
@@ -1,11 +1,3 @@
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1 -f docker/docker_yanqing_gpu/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream-gpu:$1 -f docker/stream_yanqing_gpu/Dockerfile .
-sed -i "s/cpu/cu100/g" .dockerignore
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1 -f docker/docker_yanqing/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream:$1 -f docker/stream_yanqing/Dockerfile .
-sed -i "s/cu100/cpu/g" .dockerignore
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/textgen-docker:$1 -f docker/Dockerfile .
 
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-docker-gpu:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/pytorch-stream-gpu:$1
+docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/textgen-docker:$1
