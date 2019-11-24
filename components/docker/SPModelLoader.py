1f9c3084d4bb9e615c9fc24380a2468ad89bc8e7
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-24 13:39:48 +0800

    gpt-2 model loader

---
+++ components/docker/SPModelLoader.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.app import app
+from suanpan.docker.arguments import Folder, String
+from suanpan.storage import StorageProxy
+
+
+@app.param(String(key="models", default="prose", help="prose,"))
+@app.param(String(key="storageType", default="oss"))
+@app.output(Folder(key="outputModel"))
+def SPModelLoader(context):
+    args = context.args
+
+    storage = StorageProxy(None, None)
+    storage.setBackend(type=args.storageType)
+
+    storage.download("common/model/gpt-2/"+args.models, args.outputModel)
+
+    return args.outputModel
+
+
+if __name__ == "__main__":
+    SPModelLoader()  # pylint: disable=no-value-for-parameter
