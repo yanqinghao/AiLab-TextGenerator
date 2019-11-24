75e27b41674e3db25573c054536586e7a40950c7
YanQinghao <woshiyanqinghao@gmail.com>
2019-11-24 13:45:34 +0800

    fix docker file

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,3 @@
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/textgen-docker:$1 -f docker/Dockerfile .
+docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/textgen-docker:$1 -f docker/docker/Dockerfile .
 
 docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/textgen-docker:$1
