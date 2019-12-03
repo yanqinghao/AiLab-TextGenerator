82a0609723842bdfc14b052caec2a69e715e1057
YanQinghao <woshiyanqinghao@gmail.com>
2019-12-03 21:48:45 +0800

    merge with model output

--- utils/bosh_generator.py
+++ utils/bosh_generator.py
@@ -38,21 +38,57 @@ def make_famous():
 
 
 def make_paragraph():
-    line = ". "
+    line = "。"
     line += "\r\n"
     line += "    "
     return line
 
 
-def make_bullshit(topic):
-    tmp = str()
-    while len(tmp) < 6000:
-        branch = random.randint(0, 100)
-        if branch < 5:
-            tmp += make_paragraph()
-        elif branch < 20:
-            tmp += make_famous()
-        else:
-            tmp += next(bosh_next)
-    tmp = tmp.replace("x", topic)
-    return tmp
+def make_bullshit(topic, length, samples):
+    output = []
+    for i in range(samples):
+        tmp = str()
+        while len(tmp) < length:
+            branch = random.randint(0, 100)
+            if branch < 5:
+                tmp += make_paragraph()
+            elif branch < 20:
+                tmp += make_famous()
+            else:
+                tmp += next(bosh_next)
+        tmp = tmp.replace("x", topic)
+        output.append(tmp)
+    return output
+
+
+def merge_bullshit(samples, length):
+    output = []
+    for text in samples:
+        topics = []
+        for i in re.split("[\n。]", text):
+            if i.endswith("。"):
+                topics.append(i)
+            else:
+                topics.append(i + "。")
+        tmp = str()
+        while len(tmp) < length:
+            branch = random.randint(0, 100)
+            if branch < 5:
+                tmp += make_paragraph()
+            elif branch < 20:
+                tmp += make_famous()
+            else:
+                tmp += next(bosh_next)
+        tmp = re.split("x", tmp)
+        tmp_res = []
+        for i, j in enumerate(tmp):
+            if i == len(tmp)-1:
+                tmp_res.append(j)
+            else:
+                tmp_res.append(j)
+                if i <= len(topics)-1:
+                    tmp_res.append(topics[i].split("。")[0])
+                else:
+                    tmp_res.append(topics[0].split("。")[0])
+        output.append("".join(tmp_res))
+    return output
